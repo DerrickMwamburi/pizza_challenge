3a534edaa0ffd2788bdc72b1d87f254a06f40cac
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-06-18 08:07:47 +0300

    config.py

--- server/config.py
+++ server/config.py
@@ -1,5 +1,13 @@
-import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///pizza_restaurant.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api(app)
